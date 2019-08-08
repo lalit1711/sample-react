4f82939042da8f23c1af6dd436672bec59b36182
lalittemm7707 <lalit.sahrma.ls45@gmail.com>
2019-08-08 15:46:43 +0530

    Update customLogic.js

--- src/customLogic.js
+++ src/customLogic.js
@@ -1,7 +1,5 @@
 export class CustomLogic {
-    slice = (_data, val) => { return _data.length > val ? _data.substr(0, val - 3) : _data }
-    
-
-    removeExtra = (_data, _char) => { return _data.replace(new RegExp(_char, "g"), ""); }
+    slice = (_data, val) => { return _data.length > val ? _data.substr(0, val - 3) : _data };
+    removeExtra = (_data, _char) => { return _data.replace(new RegExp(_char, "g"), ""); };
 }
 export default CustomLogic;

7bf5b21c225d3cc34e9b3bbd43e59c7b2a1edce1
lalittemm7707 <lalit.sahrma.ls45@gmail.com>
2019-08-10 21:44:12 +0530

    search feature

--- src/confrence.js
+++ src/confrence.js
@@ -2,6 +2,7 @@ import React, { Component} from 'react';
 import './App.css';
 import ApiService from './api.service.ts';
 import Tile from './tile.js';
+import { CustomLogic } from "./customLogic.directive.ts";
 import 'bootstrap/dist/css/bootstrap.css';
 class Conference extends Component {
   constructor(props){ 
@@ -16,12 +17,25 @@ class Conference extends Component {
       .subscribe(res => {
         if(res.status === 200) {
           this.setState({
-            _conference: res.response.free
+            _conference: res.response.free,
+            _orginalData: res.response.free
           })
         }
       });
 
   }
+
+  filterConferences = (_keyText) => {
+    const _customLogic = new CustomLogic();
+    _customLogic.filterConferences(_keyText, this.state._orginalData).then(
+      (fromResolve) => {
+        this.setState({
+          _conference: fromResolve
+        })
+      }
+    )
+  }
+
   render(){
     let _conferenceNumber = this.state._conference;
     if(!_conferenceNumber.length){
@@ -30,14 +44,23 @@ class Conference extends Component {
       )
     }
     return(
-      <div className="row">
-        {
-          _conferenceNumber.map((val,index) => {
-            return(
-              <Tile name={val.confName} image={val.imageURL} date={val.confStartDate} city={val.city} _key={val.conference_id} key={val.conference_id}> </Tile>
-            )
-          })
-        }
+      <div className="container">
+        <br />
+        <div className="row">
+          <div className="col-md-12">
+            <input type="text" placeholder="search..." onChange ={((text) => {this.filterConferences(text.target.value)})} />
+            <label>({this.state._conference.length})</label>
+          </div>
+        </div>
+        <div className="row">
+          {
+            _conferenceNumber.map((val,index) => {
+              return(
+                <Tile name={val.confName} image={val.imageURL} date={val.confStartDate} city={val.city} _key={val.conference_id} key={val.conference_id}> </Tile>
+              )
+            })
+          }
+        </div>
       </div>
     );
   }
--- src/customLogic.directive.ts
+++ src/customLogic.directive.ts
@@ -1,5 +1,12 @@
 export class CustomLogic {
     slice = (_data: string, val: number): string => {return _data.length > val ? _data.substr(0, val - 3) + "..."  : _data };
     removeExtra = (_data: string, _char: string): string => { return _data.replace(new RegExp(_char, "g"), ""); };
+    filterConferences = (_key: string, _data: any) => {
+        return new Promise((resolve, reject) => {
+            resolve(_data.filter(
+                ((cd: any) => cd.confName.toLowerCase().indexOf(_key) > -1)
+            ))
+        })
+    }
 }
 export default CustomLogic;
